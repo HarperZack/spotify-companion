e315ec0884bba891802c62e6d1bb9ec49f96df58
HarperZack <harper.zack@gmail.com>
2020-07-30 13:39:50 -0400

    finished artist search as well as basic test suite

--- spotify_api.py
+++ spotify_api.py
@@ -6,9 +6,10 @@ import pprint
 
 
 CLIENT_ID = 'f970d68b72c642389ed67f78bdecbaa4'
-CLIENT_SECRET = '828f7c6f24be48568803e601d2814d76'
+CLIENT_SECRET = 1
 CLIENT_CREDS = base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode())
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
+LIMIT_DEFAULT = '1'
 
 
 def now():
@@ -48,36 +49,85 @@ class SpotifyAPI:
             seconds=expires_in)
         return expires < datetime.datetime.now()
 
-    def test_search(self, track='Time'):
-        data = urlencode({'q': f'{track}', 'type': 'track'})
-        endpoint = f'{self.base_url}{self.version}search'
-        url_query = f'{endpoint}?{data}'
-        return requests.get(url_query, headers=self.header).json()
-
     """Searching functions"""
 
-    def get_resource(self, lookup_id, resource_type):
-        endpoint = f'{self.base_url}{self.version}{resource_type}/{lookup_id}'
-        response = requests.get(endpoint, headers=self.header)
-        if response.status_code not in range(200, 299):
-            raise Exception('Response failed; check input types')
-        return response.json()
+    def search_album_by_artist(self, album_name, artist_name):
+        pass
+        # resp = self.search(query= album_name,
 
-    def search_album(self, album_id):
-        return self.get_resource(album_id, 'albums')
+    def search_artist(self, artist_name, limit=LIMIT_DEFAULT):
+        resp = self.search(query=artist_name, resource_type='artist',
+                           limit=limit)
+        genres = resp['artists']['items'][0]['genres']
+        info_dict = {'name': resp['artists']['items'][0]['name'],
+                     'popularity': resp['artists']['items'][0]['popularity'],
+                     'genres': genres}
+        return info_dict
 
-    def search_artist(self, artist_id):
-        return self.get_resource(artist_id, 'artists')
+    def search(self, query=None, resource_type=None,
+               operator=None, operator_query=None, limit=LIMIT_DEFAULT):
+        if query is None or resource_type is None:
+            raise Exception('Query or resource type not filled')
+        if isinstance(query, dict):
+            # Puts query in form of a list to be parsed properly in next line
+            query_list = [f'{k}:{v}' for k, v in query.items()]
+            query = " ".join(query_list)
+        if operator is not None and operator_query is not None:
+            if operator.lower() == 'or' or operator.lower() == 'not':
+                operator = operator.upper()
+                if isinstance(operator_query, str):
+                    query = f'{query} {operator} {operator_query}'
+        data = urlencode({'q': query, 'type': resource_type.lower(),
+                          'limit': f'{limit}'})
+        return self.base_search(data)
 
-# https://www.youtube.com/watch?v=xdq6Gz33khQ&t=50m40s
+    def base_search(self, query):
+        # Puts query in endpoint; helper function
+        endpoint = f'{self.base_url}{self.version}search'
+        url_query = f'{endpoint}?{query}'
+        resp = requests.get(url_query, headers=self.header)
+        if resp.status_code in range(200, 299):
+            return resp.json()
+        else:
+            raise Exception('Issues with given query parameters')
+
+    """Test functions"""
+    def run_test_suite(self):
+        responses = [
+            self.test_token_expiration(),
+            self.test_base_search(),
+            self.test_search_artist()
+        ]
+        return print(responses)
+
+    def test_token_expiration(self):
+        if self.full_token['access_token'] != '':
+            response = 'Token authenticated'
+        else:
+            response = 'Token not authenticated'
+        return print(response)
+
+    def test_base_search(self):
+        search_resp = self.search('Danger Zone', 'track')
+        danger_zone = 'Danger Zone - From "Top Gun" Original Soundtrack'
+        dz_check = search_resp['tracks']['items'][0]['name']
+        kenny = 'Kenny Loggins'
+        kenny_check = search_resp['tracks']['items'][0]['artists'][0]['name']
+        if dz_check == danger_zone and kenny_check == kenny:
+            return print('Search functioning')
+        else:
+            return print('Issue with base_search()')
+
+    def test_search_artist(self):
+        search_response = self.search_artist('Kenny Loggins')
+        genres = len(search_response['genres'])
+        if search_response['name'] == 'Kenny Loggins' and genres == 5:
+            return print('Filtering success')
+        else:
+            raise Exception('Issues with artist_search()')
 
 
-t = SpotifyAPI()
-
-
-def test(tester):
-    return print(tester.full_token)
-
+test = SpotifyAPI()
 
 if __name__ == '__main__':
-    test(t)
+    test.run_test_suite()
