ee127103bd9aa3558fdfb371b5b2f261cfaf0c99
Zack <harper.zack@gmail.com>
2022-07-13 11:00:59 -0400

    Refactored into spotipy and made a new run at it with different testing classes.

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
+import spotipy_api
 import spotipy_api as spot
 import setlist_parser
 from datetime import datetime
-import os
-#
-# spotify = spotify_api.SpotifyAPI
 
 
 if __name__ == '__main__':
     # Check app's authorization and accuracy
     client = spot.SpotifyClient()
+    test = spotipy_api.Tests()
 
-    parsed_info = setlist_parser.PlaylistInfo()
-    searches = parsed_info.queries
-    title = parsed_info.playlist_title
-    now = datetime.now().strftime('%I:%M:%S %p')
+    if test.run() is True:
+        parsed_info = setlist_parser.PlaylistInfo()
+        searches = parsed_info.queries
+        title = parsed_info.playlist_title
+        now = datetime.now().strftime('%I:%M:%S %p')
 
-    print(title)
-    print(searches)
-    print(now)
+        print(title)
+        print(searches)
+        print(now)
+    else:
+        print('Check test class')
 
+    client.search_track('billie jean')
--- spotipy_api.py
+++ spotipy_api.py
@@ -1,5 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from pprint import pprint
 
 CLIENT_ID = 'b548bbde8d88420aa1b7229742ebcd2a'
 CLIENT_SECRET = '863dd96c91f1428ca2d869b2c7097620'
@@ -12,6 +13,38 @@ class SpotifyClient:
     def __init__(self):
         self.client = spotipy.Spotify(auth_manager=SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET))
 
-    def viability_check(self):
-        pass
+    """
+    Helper Methods for track searching
+    """
+    def get_name(self, results):
+        return results['tracks']['items'][0]['name']
+
+    def get_artist(self, results):
+        return results['tracks']['items'][0]['artists'][0]['name']
+
+    def search_track(self, name):
+        results = self.client.search(name)
+        pprint(self.get_name(results))
+
+    def search_artist(self, name):
+        results = self.client.search(name)
+        pprint(self.get_artist(results))
+
+
+class Tests:
+    def __init__(self):
+        self.client = SpotifyClient()
+        self.Billy_Joel = 'Billie Joel'
+        self.Billy_Shears = 'With a Little Help From My Friends'
+        self.Billy_Joe = 'Green Day'
+        self.Billie_Jean = 'Billie Jean'
+
+    def run(self):
+        if self.client.get_name(self.Billie_Jean) is self.Billie_Jean:
+            if self.client.get_name(self.Billy_Shears) is self.Billy_Shears:
+                if self.client.get_artist(self.Billy_Joel) is self.Billy_Joel:
+                    if self.client.get_artist(self.Billy_Joe) == 'Green Day':
+                        return True
+        else:
+            return False
 
